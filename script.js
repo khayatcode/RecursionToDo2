101312dc21e25f3e6e6c567b09ab1665b143aacf
khayatcode <a.khayatcode0103@gmail.com>
2023-07-01 16:44:03 -0600

    Second Commit

--- script.js
+++ script.js
@@ -23,3 +23,21 @@ console.log("Test 3");
 console.log(rBinarySearch([1,3,5,6], 5));
 console.log("Test 4");
 console.log(rBinarySearch([4,5,6,8,12], "hello"));
+
+// Greatest Common Factor
+
+function rGCF(param1, param2) {
+    if (param1 == param2) {
+        return param1;
+    }
+    if (param1 > param2) {
+        return rGCF(param1-param2, param2);
+    }
+    if (param1 < param2) {
+        return rGCF(param1, param2-param1);
+    }
+}
+
+console.log(rGCF(3, 7));   
+console.log(rGCF(18, 27)); 
+console.log(rGCF(49, 14)); 
